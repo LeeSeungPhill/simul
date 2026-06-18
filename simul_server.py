3c37b738de6c32f237d7f451e7358be5b8a7dd22
LeeSeungPhill <125461810+LeeSeungPhill@users.noreply.github.com>
2026-06-18 14:43:05 +0900

    1

--- simul_server.py
+++ simul_server.py
@@ -753,7 +753,7 @@ def stock_info():
                     SELECT market_ratio, %s AS dt FROM "stockFundMng_stock_fund_mng" WHERE acct_no = '74346047' 
                     UNION ALL
                     SELECT market_ratio, dt FROM dly_acct_balance WHERE acct = '74346047' AND dt = %s
-                    ORDER BY dt DESC LIMIT 1
+                    ORDER BY dt LIMIT 1
                 """, (prev_biz, prev_biz,))
             else:
                 # buy_date 미입력 시 최신 레코드
